7ddea9645f02c96b3e783ecb14eee2991df2c98b
Jordan Smart <jtrentsmart@gmail.com>
2025-04-25 15:57:54 -0500

    Wings

--- RCAIDE/Library/Components/Wings/Wing.py
+++ RCAIDE/Library/Components/Wings/Wing.py
@@ -1,169 +1,98 @@
-# RCAIDE/Compoments/Wings/Wing.py
-# (c) Copyright 2023 Aerospace Research Community LLC
-# 
-# Created: Jul 2024, RCAIDE Team
+# RCAIDE/Framework/Components/Wing.py
+# (c) Copyright 2024 Aerospace Research Community LLC
+#
+# Created: Sep, 2024, RCAIDE Team
 
 # ----------------------------------------------------------------------------------------------------------------------
-#  IMPORT
-# ---------------------------------------------------------------------------------------------------------------------- 
-# RCAIDE imports    
-from RCAIDE.Framework.Core import Data,Container
-from RCAIDE.Library.Components import Mass_Properties, Component  
+# IMPORT 
+# ----------------------------------------------------------------------------------------------------------------------
 
 import numpy as np
 
-# ---------------------------------------------------------------------------------------------------------------------- 
-#  Wing
-# ----------------------------------------------------------------------------------------------------------------------   
-class Wing(Component):
-    """Default wing compoment class. 
-    """      
-    def __defaults__(self):
-        """This sets the default values of a wing defined in RCAIDE.
-    
-        Assumptions:
-            None
+from dataclasses import dataclass, field, make_dataclass
 
-        Source:
-            None 
-        """         
+import RCAIDE.Framework as rcf
 
-        self.tag                               = 'wing'
-        self.mass_properties                   = Mass_Properties()
-        self.origin                            = np.array([[0.0,0.0,0.0]])
-                                               
-        self.symmetric                         = True
-        self.vertical                          = False
-        self.t_tail                            = False
-        self.taper                             = 0.0
-        self.dihedral                          = 0.0
-        self.aspect_ratio                      = 0.0
-        self.thickness_to_chord                = 0.0
-        self.aerodynamic_center                = [0.0,0.0,0.0]
-        self.percent_span_root_offset          = 0.0 
-        self.total_length                      = 0.0
-                                               
-        self.spans                             = Data()
-        self.spans.projected                   = 0.0
-        self.spans.total                       = 0.0
-                                               
-        self.areas                             = Data()
-        self.areas.reference                   = 0.0
-        self.areas.exposed                     = 0.0
-        self.areas.affected                    = 0.0
-        self.areas.wetted                      = 0.0
-                                               
-        self.chords                            = Data()
-        self.chords.mean_aerodynamic           = 0.0
-        self.chords.mean_geometric             = 0.0
-        self.chords.root                       = 0.0
-        self.chords.tip                        = 0.0
-                                               
-        self.sweeps                            = Data()
-        self.sweeps.quarter_chord              = 0.0
-        self.sweeps.leading_edge               = None
-        self.sweeps.half_chord                 = 0.0        
-                                               
-        self.twists                            = Data()
-        self.twists.root                       = 0.0
-        self.twists.tip                        = 0.0
-                                               
-        self.high_lift                         = False
-        self.symbolic                          = False 
-        self.high_mach                         = False
-        self.vortex_lift                       = False
-                                               
-        self.transition_x_upper                = 0.0
-        self.transition_x_lower                = 0.0
-                                               
-        self.Airfoil                           = Container()  
-        
-        self.Segments                          = Container()
-        self.control_surfaces                  = Container() 
-
-    def append_segment(self,segment):
-        """ Adds a segment to the wing 
-    
-        Assumptions:
-            None
-
-        Source:
-            None
-
-        Args:
-            self    (dict): wing data structure
-            segment (dict): wing segment data structure
-
-        Returns:
-            None 
-        """ 
-
-        # Assert database type
-        if not isinstance(segment,Data):
-            raise Exception('input component must be of type Data()')
-
-        # Store data
-        self.Segments.append(segment)
-
-        return
-    
-    def append_airfoil(self,airfoil):
-        """ Adds an airfoil to the segment 
-    
-        Assumptions:
-            None
-
-        Source:
-            None
-
-        Args:
-            self    (dict): wing data structure
-            airfoil (dict): airfoil segment data structure 
-
-        Returns:
-            None
-
-        """ 
-
-        # Assert database type
-        if not isinstance(airfoil,Data):
-            raise Exception('input component must be of type Data()')
-
-        # Store data
-        self.Airfoil.append(airfoil)
-
-        return        
+# ----------------------------------------------------------------------------------------------------------------------
+# Wing
+# ----------------------------------------------------------------------------------------------------------------------
 
 
-    def append_control_surface(self,control_surface):
-        """ Adds a component to vehicle 
-    
-        Assumptions:
-        None
+@dataclass(kw_only=True)
+class Wing(rcf.Component):
 
-        Source:
-        None
+    name: str = 'Wing'
+    airfoil: rcf.Components.Airfoil = field(default_factory=lambda: rcf.Components.Airfoil.NACA_4_Series(2412))
 
-        Args:
-        None
+    # Specialty Attributes
 
-        Returns:
-        None
+    symmetric: bool = True
+    vertical: bool = False
+    t_tail: bool = False
+    high_lift: bool = False
+    symbolic: bool = False
+    high_mach: bool = False
+    vortex_lift: bool = False
 
-        """ 
+    taper: float = 0.0
+    dihedral: float = 0.0
+    aspect_ratio: float = 0.0
+    thickness_to_chord: float = 0.0
+    exposed_root_chord_offset: float = 0.0
 
-        # Assert database type
-        if not isinstance(control_surface,Data):
-            raise Exception('input control surface must be of type Data()')
+    transition_x_upper: float = 0.0
+    transition_x_lower: float = 0.0
 
-        # Store data
-        self.control_surfaces.append(control_surface)
+    dynamic_pressure_ratio: float = 0.0
 
-        return 
-    
+    aerodynamic_center: np.ndarray = field(default_factory=lambda: np.array([0.0, 0.0, 0.0]))
 
-# ------------------------------------------------------------
-#  Handle Linking
-# ------------------------------------------------------------
+    spans   = rcf.ComponentDimensions()
+    chords  = rcf.ComponentDimensions()
+    twists  = rcf.ComponentDimensions()
+    sweeps  = rcf.ComponentDimensions()
 
-Wing.Container = Container
+
+    def __post_init__(self):
+
+        self.chords.mean_aerodynamic = 0.0
+        self.chords.mean_geometric = 0.0
+        self.chords.root = 0.0
+        self.chords.tip = 0.0
+
+        self.twists.root = 0.0
+        self.twists.tip = 0.0
+
+        self.sweeps.leading_edge = None
+        self.sweeps.quarter_chord = 0.0
+        self.sweeps.half_chord = 0.0
+
+
+@dataclass(kw_only=True)
+class WingSegment(rcf.Component):
+
+    name: str = 'Wing Segment'
+    airfoil: rcf.Components.Airfoil = field(default_factory=lambda: rcf.Components.Airfoil.NACA_4_Series(2412))
+
+    # Specialty Attributes
+
+    thickness_to_chord: float = 0.0
+    percent_span_location: float = 0.0
+    twist: float = 0.0
+    dihedral_outboard: float = 0.0
+
+    sweeps: rcf.ComponentDimensions()
+
+    def __post_init__(self):
+        self.sweeps.leading_edge = None
+        self.sweeps.quarter_chord = 0.0
+        self.sweeps.half_chord = 0.0
+
+@dataclass(kw_only=True)
+class WingControlSurface(rcf.Component):
+
+    name: str = 'Wing Control Surface'
+
+
+
+
